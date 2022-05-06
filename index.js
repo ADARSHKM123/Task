8cfc6cd34702ded91e03a930d77354710844321a
ADARSH <67700780+ADARSHKM123@users.noreply.github.com>
2022-05-06 20:56:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 const input1 = [ 1,2,3,4,5,6,7,8,9 ]
-// const input2= 3
+const input2= 3
 
  output: [ 4,5,6,7,8,9,1,2,3 ]
 
 // // WE can not use inbuild function of javascript 
 
-// let input2= [3,input1[1],input1[0]]
-
-// for(let i=3; i<input1.length;i++){
-//     console.log(input1[i]);
-// }
-// for(let j=input2.length-1;j>=0;j--){
-//     console.log(input2[j]);
-// }
 
 
-Array.prototype.rotate = function(n) {
-    var len = this.length;
-    return !(n % len) ? this.slice()
-                      : this.map((e,i,a) => a[(i + (len + n % len)) % len]);
-    };
-    var a = [1,2,3,4,5,6,7,8,9],
-        b = a.rotate(-1);
-        b = a.rotate(3);
-    console.log(JSON.stringify(b));
+function gcd( a, b){
+  if (b == 0)
+      return a;
+  else
+      return gcd(b, a % b);
+}
+
+
+function leftRotate(arr, d, n){
+  /* To handle if d >= n */
+  d = d % n;
+  let g_c_d = gcd(d, n);
+  for (let i = 0; i < g_c_d; i++) {
+      /* move i-th values of blocks */
+      let temp = arr[i];
+      let j = i;
+
+      while (1) {
+          let k = j + d;
+          if (k >= n)
+              k = k - n;
+
+          if (k == i)
+              break;
+
+          arr[j] = arr[k];
+          j = k;
+      }
+      arr[j] = temp;
+  }
+}
+
+
+function printArray(arr, size){
+  for (let i = 0; i < size; i++)
+     console.log(arr[i]);
+}
+
+let arr = input1;
+let n = arr.length;
+// Function calling
+leftRotate(arr, input2, n);
+printArray(arr, n);
