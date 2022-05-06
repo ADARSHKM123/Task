47c7aef02d61523aa2d101ede98728e3e46c584a
ADARSH <67700780+ADARSHKM123@users.noreply.github.com>
2022-05-06 16:16:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,22 @@ const input1 = [ 1,2,3,4,5,6,7,8,9 ]
 
 // // WE can not use inbuild function of javascript 
 
-let input2= [3,input1[1],input1[0]]
+// let input2= [3,input1[1],input1[0]]
 
-for(let i=3; i<input1.length;i++){
-    console.log(input1[i]);
-}
-for(let j=input2.length-1;j>=0;j--){
-    console.log(input2[j]);
-}
+// for(let i=3; i<input1.length;i++){
+//     console.log(input1[i]);
+// }
+// for(let j=input2.length-1;j>=0;j--){
+//     console.log(input2[j]);
+// }
+
+
+Array.prototype.rotate = function(n) {
+    var len = this.length;
+    return !(n % len) ? this.slice()
+                      : this.map((e,i,a) => a[(i + (len + n % len)) % len]);
+    };
+    var a = [1,2,3,4,5,6,7,8,9],
+        b = a.rotate(-1);
+        b = a.rotate(3);
+    console.log(JSON.stringify(b));
